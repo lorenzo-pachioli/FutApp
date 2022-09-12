e3ce6300ef5aed3620a77a04e9671619073aa0ce
lorenzo-pachioli <lorenzopachioli@gmail.com>
2022-09-12 12:04:53 -0300

    -> code cleaning, -> Error handling (temporal) for standings and fixture

--- Server/src/leagueStandings/service/StandingsService.js
+++ Server/src/leagueStandings/service/StandingsService.js
@@ -1,18 +1,25 @@
 const Standings = require('../models/StandingsModel');
 
 exports.StandingsService = async (league, res) => {
+    try {
+        Standings.findOne({ _id: league }, {}, (err, standings) => {
+            if (err) return res.json({
+                success: false,
+                msj: `Error: ${err}`,
+                content: []
+            });
 
-    Standings.findOne({ _id: league }, {}, (err, standings) => {
-        if (err) return res.json({
+            res.json({
+                success: true,
+                msj: `League ${league} standings in 2022`,
+                content: standings.standings
+            });
+        })
+    } catch (err) {
+        return res.json({
             success: false,
             msj: `Error: ${err}`,
             content: []
         });
-
-        res.json({
-            success: true,
-            msj: `League ${league} standings in 2022`,
-            content: standings.standings
-        });
-    })
+    }
 }
